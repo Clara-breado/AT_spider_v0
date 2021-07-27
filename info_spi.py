793516deb517e366e18de86f3f2a1bae7ab4ce55
Clara-breado <52954733+Clara-breado@users.noreply.github.com>
2021-07-27 10:44:46 +0800

    first version of multi thread

---
+++ info_spi.py
@@ -0,0 +1,241 @@
+import queue
+import threading
+from queue import Queue
+import time
+from lxml import etree
+import requests
+
+import traceback
+import emoji
+import requests
+import json
+import re
+
+class AT_spider:
+    def __init__(self,start_url):
+        self.start_url = start_url
+        self.page_cnt = 0
+        self.url_list = []
+        self.attr_list = []
+        self.flag = False
+        self.page_flag = False
+        self.end = False
+
+    def get_html(self,url):
+        header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36 Edg/91.0.864.71'}
+        html = requests.get(url,headers = header).text.replace('<!--red_beg-->','').replace('<!--red_end-->','').replace('<em>','').replace('</em>','')
+        
+        html = emoji.demojize(html)
+        cop = re.compile("[^\u0020-\u007e]")
+        html = cop.sub('',html)
+
+        selector = etree.HTML(html)
+        str = etree.tostring(selector)
+
+        return selector
+
+    def get_raw_html(self,url):
+        header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36 Edg/91.0.864.71'}
+        html = requests.get(url,headers = header).text.replace('<!--red_beg-->','').replace('<!--red_end-->','').replace('<em>','').replace('</em>','')
+        return html
+
+    def get_attr_url(self,city_url):
+        html = self.get_html(city_url)
+        attrs_url = html.xpath('//*[@id="lithium-root"]/main/span/div/div[2]/div/div/div/span/div/div[2]/div[2]//section[@class="_3Y-YU9SE _2gl5HHyP"]//div[@class="_1R2M9xFP"]//a/@href')
+        for i in range(len(attrs_url)):
+            attrs_url[i] = str(attrs_url[i])
+            attrs_url[i] = 'https://www.tripadvisor.in'+attrs_url[i]
+            #f.write(attrs_url[i]+"\n")
+        #f.close()
+        return attrs_url
+
+    def get_attr_info(self,attr_queue):
+         while 1:
+            if(attr_queue.empty() is True):
+                #delay for get_url
+                time.sleep(10)
+                if(attr_queue.empty() is True):
+                    self.flag = True
+                    print("---------------closed---------------")
+                    break
+            else:
+    
+                attr_url = attr_queue.get()
+                html = self.get_html(attr_url)
+                print("---------------enter---------------")
+
+                ##name
+                name = html.xpath('//*[@id="lithium-root"]/main/div[1]/div[2]/div[1]/header/div[3]/div[1]/div/h1//text()')[0]
+                cop = re.compile("['/','?',':','*','<','>','|',\u0022]")    #todo:'\'
+                name = cop.sub("",name)
+
+                print("name:%s" %name)
+
+                ##labels
+                keywords = html.xpath('//*[@id="lithium-root"]/main/div[1]/div[2]/div[2]/div/div/span/section[1]/div/div/span/div/div[1]/div[3]/div/text()')
+                keywords = re.split('&',keywords[0])
+                keywordslists = html.xpath('//*[@id="tab-data-qa-reviews-0"]/div/div[1]/div/div[3]/div[2]/*//text()')
+                keywords.extend(keywordslists)
+                ##location
+                location = html.xpath('//*[@id="tab-data-AppPresentation_PoiLocationSectionGroup"]/div/div/div[2]/div[1]/span/div/div/div[1]/button/span/text()')
+                ##score&score cnt
+                score = html.xpath('//*[@id="tab-data-qa-reviews-0"]/div/div[3]/div[1]/div[1]/div[1]/text()')[0]
+                score_cnt = html.xpath('//*[@id="tab-data-qa-reviews-0"]/div/div[3]/div[1]/div[1]/div[2]/span/text()')  #to int
+                cop = re.compile("[^0-9]")
+                score_cnt = cop.sub('',score_cnt[0])
+                ##nearby: hotels,restaurants
+                # near_hotels = html.xpath('//*[@id="answer_faq_5"]/div/div/ul//a/text()')
+                near_restas = html.xpath('//*[@id="tab-data-AppPresentation_PoiLocationSectionGroup"]/div/div/div[2]/div[3]/span[1]/div[4]//div[@class="DrjyGw-P NGv7A1lw _2yS548m8 _2cnjB3re _1TAWSgm1 _1Z1zA2gh _2-K8UW3T _2nPM5Opx"]//text()')
+                near_attras = html.xpath('//*[@id="tab-data-AppPresentation_PoiLocationSectionGroup"]/div/div/div[2]/div[3]/span[2]/div[4]//div[@class="DrjyGw-P NGv7A1lw _2yS548m8 _2cnjB3re _1TAWSgm1 _1Z1zA2gh _2-K8UW3T _2nPM5Opx"]//text()')
+                ##top ways to experience
+                #names
+                ex_names = html.xpath('//*[@id="lithium-root"]/main/div[1]/div[2]/div[2]/div/div/span/section[3]/div/div/span/div[2]//span[@class="DrjyGw-P _2AAjjcx8"]/text()')
+                extype_cnt = len(ex_names)
+                ex = ['']*extype_cnt
+                for i in range(extype_cnt):
+                    ex[i] = html.xpath('//*[@id="ATTRACTION_WAYS_TO_EXPERIENCE-tabpanel-{index}"]//li//div[@class="VQlgmkyI WullykOU _3WoyIIcL"]//text()'.format(index=i))
+
+                experience = {}
+                for i in range(extype_cnt):
+                    experience[ex_names[i]] = ex[i]
+                nearby = {}
+                # nearby['near_hotels'] = near_hotels
+                nearby['near_restas'] = near_restas
+                nearby['near_attras'] = near_attras
+
+                #turn to json
+                attraction = {}
+                attraction['name'] = name
+                attraction['labels'] = keywords
+                attraction['score'] = score
+                attraction['score_cnt'] = score_cnt
+                attraction['experiences'] = experience
+                attraction['nearby'] = nearby
+
+                lock = threading.Lock()
+                lock.acquire()
+                path = "C:\\Users\\t-dohuang\\source\\repos\\TripAdvisor_spi\\London_data\\reletive_info\\{}.json".format(name)
+                f = open(path,"a")
+                json.dump(attraction,f,indent=2)  
+                lock.release()               
+
+    def use_url(self,url_queue,attr_queue):
+        time.sleep(0.1)
+        while 1:
+            if(url_queue.empty() is True):
+                #delay for get_url
+                time.sleep(1)
+                if(url_queue.empty() is True):
+                    self.page_flag = True
+                    print("---------------use_closed---------------")
+                    break
+            else:
+                url = url_queue.get()
+                # print("取得URL：{}\n".format(url))
+                attrs_url = self.get_attr_url(url)
+                for attr_url in attrs_url:
+                    attr_queue.put(attr_url)
+                    # print(attr_url+"\n")
+                  
+
+        
+    def get_url(self,url_queue):
+        base_url = 'https://www.tripadvisor.in/Attractions-g186338-Activities-a_allAttractions.true-London_England.html'
+        # html = get_html(base_url)
+
+        # 构造所有ｕｒｌ    todo:10-->page_cnt
+        for i in range(self.page_cnt):
+            attr_url = 'https://www.tripadvisor.in/Attractions-g186338-Activities-oa{}-London_England.html'.format(i*30)
+            url_queue.put(attr_url)
+            # print("获得URL：{}".format(attr_url))    
+
+
+    def main(self):
+        start_page = self.get_html(self.start_url)
+        self.page_cnt = int(int(start_page.xpath('//*[@id="lithium-root"]/main/span/div/div[2]/div/div/div/span/div/div[2]/div[2]/section[39]/span/div[2]/div/div/text()[6]')[0].replace(',',''))/30)
+        
+        thread_pool = []
+        res_queue = Queue()
+        out_queue = Queue()
+        get_url_thread = threading.Thread(target=self.get_url,args=(res_queue,))
+        get_url_thread.daemon = True
+        get_url_thread.start()
+        # get_url_thread.join()
+        # time.sleep(15)
+        for i in range(5):
+            use_url_thread = threading.Thread(target=self.use_url,args=(res_queue,out_queue,))
+            use_url_thread.daemon = True
+            use_url_thread.start()
+            thread_pool.append(use_url_thread)
+        
+        time.sleep(30)
+        for i in range(10):
+            get_attr_thread = threading.Thread(target=self.get_attr_info,args=(out_queue,))
+            get_attr_thread.daemon = True
+            get_attr_thread.start()
+            thread_pool.append(get_attr_thread)
+
+        for thread in thread_pool:
+            thread.join()    
+
+        # while 1:
+        #     # print(self.flag)
+        #     if self.end == True:
+        #         break
+        print("threads end!!!")
+
+# def storage_url(stor_queue):
+#     while(stor_queue.empty)
+
+start = time.time()
+start_url = 'https://www.tripadvisor.in/Attractions-g186338-Activities-a_allAttractions.true-London_England.html'
+a = AT_spider(start_url)
+a.main()
+end = time.time()
+print("time:%s" %(end-start))
+
+
+# if __name__ == '__main__':
+#     print("start")
+#     start = time.time()
+#     res_queue = Queue()
+#     out_queue = Queue()
+
+#     get_url_thread = threading.Thread(target=get_url,args=(res_queue,))
+#     get_url_thread.daemon = True
+#     get_url_thread.start()
+#     time.sleep(15)
+
+#     thread_pool = []
+#     for i in range(3):
+#         use_url_thread = threading.Thread(target=use_url,args=(res_queue,out_queue,))
+#         use_url_thread.daemon = True
+#         use_url_thread.start()
+#         thread_pool.append(use_url_thread)
+
+#     for us_thread in thread_pool:
+#         us_thread.join()
+
+    # use_url_thread_1 = threading.Thread(target=use_url,args=(res_queue,out_queue,))
+    # use_url_thread_1.daemon = True
+    # use_url_thread_1.start()  
+
+    # use_url_thread_2 = threading.Thread(target=use_url,args=(res_queue,out_queue,))
+    # use_url_thread_2.daemon = True
+    # use_url_thread_2.start()  
+
+    # use_url_thread_3 = threading.Thread(target=use_url,args=(res_queue,out_queue,))
+    # use_url_thread_3.daemon = True
+    # use_url_thread_3.start()  
+
+    # storage_url_thread = threading.Thread(target=storage_url,args=(out_queue,))
+    # storage_url_thread.daemon = True
+    # storage_url_thread.start()
+
+    # time.sleep(15)  #!!!!time to wait the procs
+    # print(out_queue.qsize())
+    # while out_queue.empty() is False:
+    #     print(out_queue.get()+"\n")
+    
+    # end = time.time()
+    # print("total time:%s\n" %(end-start))
---
+++ review_spi.py
@@ -0,0 +1,351 @@
+import os
+import sys
+import logging
+import random
+import emoji
+import requests
+import json
+import re
+from lxml import etree
+import time
+import threading
+import traceback
+from queue import Queue
+from threading import Thread
+
+##one object for one attraction
+class AT_view_spider:
+    def __init__(self,base_url):
+        self.base_url = base_url
+        #start_url include format!!!
+        self.review_request_queue = Queue()
+        self.review_get_queue = Queue()
+        self.index_queue = Queue()
+        self.page_cnt = 0
+        self.review_cnt = 0
+        self.max_review = 20    #20 review in one json file
+        self.url_list = []
+        self.review_dict = []
+        self.attr_name = ''
+        self.dir_path = 'C:\\Users\\t-dohuang\\source\\repos\\TripAdvisor_spi\\London_data\\review\\{attr_name}'
+        self.save_path = 'C:\\Users\\t-dohuang\\source\\repos\\TripAdvisor_spi\\London_data\\review\\{attr_name}\\{attr_name}{index}.json'
+        self.flag = False
+        self.GET_finish = False
+        self.USE_finish = False
+        self.SAVE_finish = False
+
+    def get_html(self,url):
+        # header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36 Edg/91.0.864.71'}
+        # html = requests.get(url,headers = header).text.replace('<!--red_beg-->','').replace('<!--red_end-->','').replace('<em>','').replace('</em>','')
+        
+        # html = emoji.demojize(html)
+        # cop = re.compile("[^\u0020-\u007e]")
+        # html = cop.sub('',html)
+
+        # selector = etree.HTML(html)
+        # str = etree.tostring(selector)
+
+        # return selector
+        while True:
+            try:
+                header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36 Edg/91.0.864.71'}
+                html = requests.get(url,headers = header).text.replace('<!--red_beg-->','').replace('<!--red_end-->','').replace('<em>','').replace('</em>','')
+                
+                html = emoji.demojize(html)
+                cop = re.compile("[^\u0020-\u007e]")
+                html = cop.sub('',html)
+
+                selector = etree.HTML(html)
+                str = etree.tostring(selector)
+
+                return selector
+
+            except Exception as e:
+                logger.error(e)
+                logger.error(traceback.format_exc())
+                randomtime = random.randint(1,5)
+                logger.warn('ERROR - Retrying again website %s, retrying in %d secs' % (url, randomtime))
+                time.sleep(randomtime)
+                continue
+                # print('Error:',e)
+                # traceback.print_exc()
+
+    def get_review_url_proc(self,review_request_queue):
+        #construct urls
+        #get attr's name,construct a new file of this attr(upper level)
+        html = self.get_html(self.base_url.format(page=''))
+        self.page_cnt = int(int(html.xpath('//*[@id="tab-data-qa-reviews-0"]/div/div[5]/div[11]/div[2]/div/div/text()[6]')[0].replace(',',''))/10)
+        #first_page is diff url
+        self.attr_name = html.xpath('//*[@id="lithium-root"]/main/div[1]/div[2]/div[1]/header/div[3]/div[1]/div/h1//text()')[0]
+        cop = re.compile("['/','?',':','*','<','>','|',\u0022]")    #todo:'\'
+        self.attr_name = cop.sub("",self.attr_name)
+        #mkdir
+        self.dir_path = self.dir_path.format(attr_name=self.attr_name)
+        isExists = os.path.exists(self.dir_path)
+        if not isExists:
+            os.makedirs(self.dir_path)
+            print ('mkdir:'+self.dir_path)
+        else:
+            print('alrady exists'+self.dir_path)
+        #base_url = 'https://www.tripadvisor.in/Attraction_Review-g186338-d553603-Reviews{page}-London_Eye-London_England.html'
+        for i in range(self.page_cnt):
+            #start from sec
+            bias = (i+1)*10     #10,20...52900,52910
+            url = self.base_url.format(page='-or'+str(bias))
+            #url_list.append(url)
+            review_request_queue.put(url)
+            #single_page_proc(url,bias)
+        self.GET_finish = True
+        print("--------------GET_FINISH!!!---------------")
+
+    def single_review_proc(self,review_url,review_get_queue):      
+        html = self.get_html(review_url)
+        for i in range(10):
+            review = {}
+            review_path = '//*[@id="tab-data-qa-reviews-0"]/div/div[5]/div[{index}]'.format(index=i+1)
+
+            re_title = html.xpath(review_path+'/span/span/a/div/span/text()')
+            if(len(re_title)>0):
+                re_title = re_title[0]
+            else:
+                re_title = 'NONE'
+
+            re_time = html.xpath(review_path+'/span/span/div[4]/text()')
+            if len(re_time)==0:
+                re_time = 'NONE'
+                re_type = 'NONE'
+                re_body = html.xpath(review_path+'/span/span/div[4]/div[1]/div/span/text()')
+            else:
+                re_time = re_time[0].encode('utf-8').decode('utf-8')
+                if ('  ' in re_time) is True:
+                    [re_time,re_type] = re_time.split('  ')
+                else:
+                    re_type = 'NONE'
+                re_body = html.xpath(review_path+'/span/span/div[5]/div[1]/div/span/text()')
+            reviews = ''
+            for re in re_body:
+                re = re.encode('utf-8').decode('utf-8')
+                reviews += re
+
+            review['title'] = re_title
+            review['time'] = re_time
+            review['type'] = re_type
+            review['review'] = reviews
+            review_get_queue.put(review)
+
+    def use_review_url_proc(self,review_request_queue,review_get_queue):
+        while 1:
+            if(review_request_queue.empty() is True):
+                #delay for get_url
+                print("---------------use sleep---------------")
+                time.sleep(1)
+                if(review_request_queue.empty() is True):
+                    if(self.GET_finish is True):
+                        self.USE_finish = True
+                        print("--------------use_FINISH!!!---------------")
+                    else:
+                        print("---------------use_closed-----------------")
+                    self.flag = True
+                    break
+            else:
+                url = review_request_queue.get()
+                self.single_review_proc(url,review_get_queue)
+
+    def write_to_file(self,review_get_queue,index_queue):
+        while True:
+            if(review_get_queue.empty() is True or index_queue.empty() is True):
+                #delay for get_url
+                print("---------------write sleep---------------")
+                time.sleep(30)
+                if(review_get_queue.empty() is True or index_queue.empty() is True):
+                    if(self.USE_finish is True):
+                        self.SAVE_finish = True
+                        print("--------------write_FINISH!!!---------------")
+                    else:
+                        print("---------------closed---------------")
+                    self.flag = True   
+                    break
+            else:
+                idx = index_queue.get()
+                index_queue.put(idx+1)
+                reviews = {}
+                for i in range(self.max_review):
+                    # reviews.update(review)
+                    id = idx*self.max_review+i
+                    if(review_get_queue.empty() is False):
+                        review = review_get_queue.get()
+                        reviews[id] = review
+                    else:
+                        break
+                lock = threading.Lock()
+                lock.acquire()
+                path = self.save_path.format(attr_name=self.attr_name,index=idx)
+                print(path+"\n")
+                print("---------------------------\n")
+                f = open(path,"a")
+                json.dump(reviews,f,indent=2)
+                lock.release()
+
+    def check_get(self,sleeptimes,get_initThreadName=[]):
+        while True:
+            if(self.GET_finish is True):
+                break
+            nowThreasName=[]
+            now = threading.enumerate()
+            for i in now:
+                nowThreasName.append(i.getName())
+            for thread in get_initThreadName:
+                if thread in nowThreasName:
+                    pass
+                else:
+                    print("restart"+thread+'\n')
+                    get_url_thread = threading.Thread(target=self.get_review_url_proc,args=(self.review_request_queue,))
+                    get_url_thread.daemon =True
+                    get_url_thread.setName(thread)
+                    get_url_thread.start()
+            time.sleep(sleeptimes)
+            
+    def check_use(self,sleeptimes,use_initThreadName=[]):
+        while True:
+            if(self.USE_finish is True):
+                break
+            nowThreasName=[]
+            now = threading.enumerate()
+            for i in now:
+                nowThreasName.append(i.getName())
+            for thread in use_initThreadName:
+                if thread in nowThreasName:
+                    pass
+                else:
+                    print("restart"+thread+'\n')
+                use_url_thread = threading.Thread(target=self.use_review_url_proc,args=(self.review_request_queue,self.review_get_queue,))
+                use_url_thread.daemon = True
+                use_url_thread.setName(thread)
+                use_url_thread.start()
+            time.sleep(sleeptimes)
+
+    def check_save(self,sleeptimes,save_initThreadName=[]):
+        while True:
+            if(self.SAVE_finish is True):
+                break
+            nowThreasName=[]
+            now = threading.enumerate()
+            for i in now:
+                nowThreasName.append(i.getName())
+            for thread in save_initThreadName:
+                if thread in nowThreasName:
+                    pass
+                else:
+                    print("restart"+thread+'\n')
+                save_thread = threading.Thread(target=self.write_to_file,args=(self.review_get_queue,self.index_queue))
+                save_thread.daemon = True
+                save_thread.setName(thread)
+                save_thread.start()
+            time.sleep(sleeptimes)
+    def main(self):
+        # review_request_queue = Queue()
+        # review_get_queue = Queue()
+        # index_queue = Queue()
+        self.index_queue.put(0)
+        review_generate_dict = []
+
+        thread_pool = []
+        get_initThreadName = []
+        use_initThreadName = []
+        save_initThreadName = []
+
+        #generate reveiw urls
+        for i in range(1):
+            get_url_thread = threading.Thread(target=self.get_review_url_proc,args=(self.review_request_queue,))
+            get_url_thread.daemon =True
+            thread_name = 'get_url_{index}'.format(index=i)
+            get_url_thread.setName(thread_name)
+            get_url_thread.start()
+            thread_pool.append(get_url_thread)
+            get_initThreadName.append(get_url_thread.getName())
+
+        #crawl review url
+        time.sleep(2)
+        for i in range(10):
+            use_url_thread = threading.Thread(target=self.use_review_url_proc,args=(self.review_request_queue,self.review_get_queue,))
+            use_url_thread.daemon = True
+            thread_name = 'use_url_{index}'.format(index=i)
+            use_url_thread.setName(thread_name)
+            use_url_thread.start()
+            thread_pool.append(use_url_thread)
+            use_initThreadName.append(use_url_thread.getName())
+        
+        #save to file
+        time.sleep(10)
+        for i  in range(3):
+            save_thread = threading.Thread(target=self.write_to_file,args=(self.review_get_queue,self.index_queue))
+            save_thread.daemon = True
+            thread_name = 'save_{index}'.format(index=i)
+            save_thread.setName(thread_name)
+            save_thread.start()
+            thread_pool.append(save_thread)
+            save_initThreadName.append(save_thread.getName())
+        
+        # init = threading.enumerate()
+        # for i in init:
+        #     initThreadName.append(i.getName())
+
+
+        check_get = threading.Thread(target=self.check_get,args=(600,get_initThreadName,))
+        check_get.setName('check-GET')
+        check_get.start()
+        thread_pool.append(check_get)
+        
+        check_use = threading.Thread(target=self.check_use,args=(180,use_initThreadName,))
+        check_use.setName('check-USE')
+        check_use.start()
+        thread_pool.append(check_use)
+
+        check_save = threading.Thread(target=self.check_save,args=(60,save_initThreadName,))
+        check_save.setName('check-SAVE')
+        check_save.start()
+        thread_pool.append(check_save)
+
+        # for thread in thread_pool:
+        #     thread.join()
+
+
+        print("threads end!")
+
+start = time.time()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.INFO)
+handler = logging.FileHandler('Spider.log')
+formatter = logging.Formatter('%(asctime)s - %(lineno)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+start_url_2 = 'https://www.tripadvisor.in/Attraction_Review-g186338-d553603-Reviews{page}-London_Eye-London_England.html'
+start_url = 'https://www.tripadvisor.in/Attraction_Review-g186338-d194299-Reviews{page}-Churchill_War_Rooms-London_England.html'
+urls = []
+urls.append('https://www.tripadvisor.in/Attraction_Review-g186338-d211708-Reviews{page}-Houses_of_Parliament-London_England.html')
+urls.append('https://www.tripadvisor.in/Attraction_Review-g186338-d211709-Reviews{page}-Big_Ben-London_England.html')
+urls.append('https://www.tripadvisor.in/Attraction_Review-g186338-d218015-Reviews{page}-Westminster-London_England.html')
+
+
+# a = AT_view_spider(urls[0])
+# a.main()
+
+# a = AT_view_spider(start_url)
+# a.main()
+sp = []
+for url in urls:
+    s = AT_view_spider(url)
+    sp.append(s)
+run_thread_pool = []
+for s in sp:
+    run_thread = threading.Thread(target=s.main(),args=())
+    run_thread.start()
+    run_thread_pool.append(run_thread)
+for thread in run_thread_pool:
+    thread.join()
+
+end = time.time()
+print("time:%s" %(end-start))
+#todo folder create
